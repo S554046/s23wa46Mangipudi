09cfacb2377ccb02708721199a34e34442b174bc
S554046 <S554046@nwmissouri.edu>
2023-03-29 23:12:04 -0500

    add mydata files to repo

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Hashwanth Gogineni' });
+  res.render('mydata', { title: 'Mangipudi Rohit' });
 });
 
 module.exports = router;
